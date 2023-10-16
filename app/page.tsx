6906c8cf06deccfbb72c6dc539d54cf47a2758de
Nguyễn Văn Hiển <hiennv@mirabo-global.com>
2023-10-17 00:18:22 +0700

    first setup project nextjs

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import ProductCart from "./components/ProductCart";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <Link href="/users">User</Link>
+      <ProductCart/>
+    </main>
+  );
+}
