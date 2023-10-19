97847e7af4f7f8c38426c942e0adcb84007bbc78
Nguyễn Văn Hiển <hiennv@mirabo-global.com>
2023-10-19 23:39:34 +0700

    complete signin signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,81 @@
-export default function Home() {
+"use client";
+import { Button } from "react-bootstrap";
+import styles from "../styles/app.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export default function Login() {
+  const router = useRouter();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/user/login`,
+        {
+          email: data.email,
+          password: data.password,
+        }
+      );
+      const currentUser = response.data?.user;
+      if (response.data?.status === true && currentUser) {
+        toast.success("You have successfully loginned");
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+        router.push("/admin");
+      } else {
+        toast.error("Incorrect email or password information");
+      }
+    } catch (error) {
+      throw new Error("error");
+    }
+  };
   return (
-    <main>
-      <h1>HOME</h1>
+    <main className={styles["container"]}>
+      <div className={styles["text-container"]}>
+        <p className={styles["text"]}>Login</p>
+      </div>
+      <div className={styles["inputs-container"]}>
+        <div className={styles["inputs"]}>
+          <label>Your email:</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={data.email}
+            onChange={(e) =>
+              setData((prevData) => ({ ...prevData, email: e.target.value }))
+            }
+          />
+        </div>
+        <div className={styles["inputs"]}>
+          <label>Your password:</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={data.password}
+            onChange={(e) =>
+              setData((prevData) => ({
+                ...prevData,
+                password: e.target.value,
+              }))
+            }
+          />
+        </div>
+      </div>
+      <div className={styles["forgot-password"]}>
+        <Link href={"forgotpassword"}>Forgot password?</Link>
+      </div>
+      <Button style={{ width: "100%" }} onClick={() => handleLogin()}>
+        Login
+      </Button>
+      <div className={styles["text-last-container"]}>
+        <p>Haven`t you already account?</p>
+        <Link href={"signup"}>Sign up</Link>
+      </div>
     </main>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { Button } from "react-bootstrap";
+import styles from "../../styles/app.module.css";
+import Link from "next/link";
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+export default function Signup() {
+  const router = useRouter();
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/user/register`,
+        {
+          username: data.username,
+          email: data.email,
+          password: data.password,
+        }
+      );
+      if (response.data?.status === true) {
+        toast.success("Congratulation!!! You have successfully registered.");
+        router.replace("/");
+      }
+    } catch (error) {
+      throw new Error("error");
+    }
+  };
+  return (
+    <>
+      <main className={styles["container"]}>
+        <div className={styles["text-container"]}>
+          <p className={styles["text"]}>Sign Up</p>
+        </div>
+        <div className={styles["inputs-container"]}>
+          <div className={styles["inputs"]}>
+            <label>Your username:</label>
+            <input
+              type="text"
+              placeholder="Username"
+              value={data.username}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  username: e.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className={styles["inputs"]}>
+            <label>Your email:</label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={data.email}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  email: e.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className={styles["inputs"]}>
+            <label>Your password:</label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={data.password}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  password: e.target.value,
+                }))
+              }
+            />
+          </div>
+        </div>
+        <Button
+          style={{ width: "100%", marginTop: "30px" }}
+          onClick={() => handleSignUp()}
+        >
+          Sign up
+        </Button>
+        <div className={styles["text-last-container"]}>
+          <p>Are you already account?</p>
+          <Link href={"/"}>Sign in</Link>
+        </div>
+      </main>
+    </>
+  );
+}
